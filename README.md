58cd17d9c467e561591f01c51684a29eb732bc9f
bingohuang <me@bingohuang.com>
2016-11-16 10:29:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 一步步带你构建 Spring Boot + Docker 应用及网易蜂巢服务
---
+++ src/main/java/com/163/c/hello/Application.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Sprint Boot 主应用入口
+ * @author <a href="http://bingohuang.com">bingohuang.com</a>
+ * @date 2016.11.15
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Spring Boot, Docker and CloudComb!";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
